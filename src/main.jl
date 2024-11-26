a04dced9b5f9356dd5f58f564a2311d9b32fa028
Gabriel Pereira <berkpereira@gmail.com>
2024-11-26 20:42:28 +0000

    minor changes

--- src/main.jl
+++ src/main.jl
@@ -3,10 +3,16 @@ include("problem_data.jl")
 include("solver.jl")
 include("printing.jl")
 
+using Revise
 using .PrototypeMethod
 using BenchmarkTools
 using Printf
 using Plots
+using SparseArrays
+plotlyjs();
+
+# Set default plot size (in pixels)
+default(size=(800, 700));
 
 ############################## FIND PROBLEMS ###################################
 
@@ -31,7 +37,7 @@ using Plots
 # problem_name = "HUESTIS";
 
 problem_set = "sslsq";
-problem_name = "NYPA_Maragal_5_lasso"
+problem_name = "NYPA_Maragal_5_lasso";
 # problem_name = "HB_ash219_lasso" # pretty good solution from prototype
 # problem_name = "HB_ash85_huber" # very slow convergence w prototype
 # problem_name = "HB_ash85_lasso" # stagnates badly at about 2e-3 objective error from SCS solution
@@ -42,6 +48,9 @@ problem_name = "NYPA_Maragal_5_lasso"
 data = load_clarabel_benchmark_prob_data(problem_set, problem_name);
 P, c, A, b, m, n, K = data.P, data.c, data.A, data.b, data.m, data.n, data.K;
 
+# Create a problem instance.
+problem = PrototypeMethod.QPProblem(data.P, data.c, data.A, data.b, data.K);
+
 ############################### SCS SOLUTION ###################################
 
 using SCS
@@ -49,7 +58,7 @@ using SCS
 println("\nRunning SCS...")
 
 # Define the SCS optimizer
-model = Model(SCS.Optimizer)
+model = Model(SCS.Optimizer);
 # set_silent(model) # Suppress output
 # Define primal variables x and s
 @variable(model, x_scs[1:n]);    # Primal variables in R^n
@@ -59,7 +68,7 @@ model = Model(SCS.Optimizer)
 @constraint(model, con, A * x_scs + s_scs .== b);
 
 # Add cone constraints to the model
-add_cone_constraints_scs!(model, s_scs, K)
+add_cone_constraints_scs!(model, s_scs, K);
 
 # Add the quadratic objective to the model
 @objective(model, Min, 0.5 * dot(x_scs, P * x_scs) + dot(c, x_scs));
@@ -72,7 +81,7 @@ x_scs = value.(x_scs);
 s_scs = value.(s_scs);
 y_scs = dual.(con);  # Dual variables (Lagrange multipliers)
 obj_scs = objective_value(model);
-;
+
 
 ############################# PROTOTYPE SOLUTION ###############################
 
@@ -82,46 +91,45 @@ obj_scs = objective_value(model);
 
 # Choose algorithm based on M1 construction
 # int in {1, 2, 3, 4}
-variant = 2
-A_gram = A' * A
+variant = 1;
+A_gram = A' * A;
 take_away = take_away_matrix(variant, A_gram);
 
-max_iters = 500;
-print_modulo = 10;
+MAX_ITERS = 600;
+PRINT_MOD = 50;
+RESTART_PERIOD = Inf;
+RETURN_RUN_DATA = true;
 
 # Choose primal step size as a proportion of maximum allowable to keep M1 PSD
-max_τ = 1 / dom_λ_power_method(Matrix(take_away), 10)
+max_τ = 1 / dom_λ_power_method(Matrix(take_away), 30);
 τ = 0.9 * max_τ
 
-# Create a problem instance
-problem = PrototypeMethod.QPProblem(data.P, data.c, data.A, data.b, data.K);
+println("Running prototype variant $variant...")
 
-# Initialize sequences x, s, and y
 x = zeros(n);
 s = zeros(m);
 y = zeros(m);
 
-# Initialise metrics storage
-# obj_vals = Float64[];
-# primal_residuals = Float64[];
-# dual_residuals = Float64[];
-
-# Main iteration loop
-println("Running prototype variant $variant...")
-
-PrototypeMethod.optimise!(problem, variant, x, s, y, τ, ρ,
-A_gram, max_iters, print_modulo, 50);
+if RETURN_RUN_DATA
+    primal_objs, dual_objs, primal_residuals, dual_residuals, x_step_angles, s_step_angles, y_step_angles = PrototypeMethod.optimise!(problem, variant, x, s, y, τ, ρ, A_gram, MAX_ITERS, PRINT_MOD, RESTART_PERIOD, Inf, RETURN_RUN_DATA);
+else
+    final_primal_obj, final_dual_obj = PrototypeMethod.optimise!(problem, variant, x, s, y, τ, ρ, A_gram, MAX_ITERS, PRINT_MOD, RESTART_PERIOD, Inf, RETURN_RUN_DATA);
+end;
 
 
 ################################################################################
 ################################# PLOT STUFF ###################################
 ################################################################################
 
-# Plot objective values
-plot(0:max_iters, obj_vals, label="Prototype Objective", xlabel="Iteration", ylabel="Objective Value", title="Variant $variant: Objective")
+display(plot(0:MAX_ITERS, primal_objs, label="Prototype Objective", xlabel="Iteration", ylabel="Objective Value", title="Variant $variant: Objective. Restart period = $RESTART_PERIOD"))
 
-# Plot primal residuals
-plot(0:max_iters, primal_residuals, label="Prototype Residual", xlabel="Iteration", ylabel="Primal Residual", title="Variant $variant: Primal Residual Norm")
+display(plot(0:MAX_ITERS, dual_objs, label="Prototype Dual Objective", xlabel="Iteration", ylabel="Dual Objective Value", title="Variant $variant: Dual Objective. Restart period = $RESTART_PERIOD"))
+
+display(plot(0:MAX_ITERS, primal_residuals, label="Prototype Residual", xlabel="Iteration", ylabel="Primal Residual", title="Variant $variant: Primal Residual Norm. Restart period = $RESTART_PERIOD"))
+
+display(plot(0:MAX_ITERS, dual_residuals, label="Prototype Dual Residual", xlabel="Iteration", ylabel="Dual Residual", title="Variant $variant: Dual Residual Norm. Restart period = $RESTART_PERIOD"))
+
+# TODO: Plot angles between consecutive steps
 
 ############################# ANALYSE RESIDUAL DATA ############################
 
