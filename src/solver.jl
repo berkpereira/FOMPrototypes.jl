651c83505748774732bdd4ea6fb42d743d68e069
Gabriel Pereira <berkpereira@gmail.com>
2024-11-27 14:12:50 +0000

    fixed bug with size(A) dimensions assignment

--- src/solver.jl
+++ src/solver.jl
@@ -83,15 +83,22 @@ function iter_y!(y::AbstractVector{Float64},
     end
 end
 
+"""
+Run the optimiser for the initial inputs and solver options given.
+
+    restart_period: Integer or Symbol
+        If an integer, the number of iterations between restarts. The Only
+        valid symbolic input is :adaptive, with obvious meaning.
+"""
 function optimise!(problem::QPProblem, variant::Integer, x::Vector{Float64},
     s::Vector{Float64}, y::Vector{Float64}, τ::Float64, ρ::Float64,
     A_gram::AbstractMatrix, max_iter::Integer, print_modulo::Integer,
-    restart_period::Real = Inf, residual_norm::Real = Inf,
+    restart_period::Union{Real, Symbol} = Inf, residual_norm::Real = Inf,
     return_run_data::Bool = false)
 
     # Unpack problem data for easier reference
     P, c, A, b, K = problem.P, problem.c, problem.A, problem.b, problem.K
-    n, m = size(A)
+    m, n = size(A)
 
     # Compute the (fixed) matrix that premultiplies the x step
     pre_matrix = pre_x_step_matrix(variant, P, A_gram, τ, ρ, n)
@@ -116,6 +123,9 @@ function optimise!(problem::QPProblem, variant::Integer, x::Vector{Float64},
         x_step_angles = Float64[]
         s_step_angles = Float64[]
         y_step_angles = Float64[]
+        concat_step_angles = Float64[]
+        # NOTE: this vector normalises the vectors
+        normalised_concat_step_angles = Float64[]
         primal_obj_vals = Float64[]
         dual_obj_vals = Float64[]
         primal_res_norms = Float64[]
@@ -123,13 +133,18 @@ function optimise!(problem::QPProblem, variant::Integer, x::Vector{Float64},
         x_step = zeros(Float64, n)
         s_step = zeros(Float64, m)
         y_step = zeros(Float64, m)
+        concat_step = zeros(Float64, n + 2 * m)
+        normalised_concat_step = zeros(Float64, n + 2 * m)
         x_step_prev = zeros(Float64, n)
         s_step_prev = zeros(Float64, m)
         y_step_prev = zeros(Float64, m)
+        concat_step_prev = zeros(Float64, n + 2 * m)
     else
         x_step_prev = nothing
         s_step_prev = nothing
         y_step_prev = nothing
+        concat_step_prev = nothing
+        normalised_concat_step_prev = nothing
     end
 
     for k in 0:max_iter
@@ -176,25 +191,40 @@ function optimise!(problem::QPProblem, variant::Integer, x::Vector{Float64},
             push!(dual_res_norms, curr_dual_res_norm)
         end
 
-        # Iterate.
+        # Iterate and record step vectors if return_run_data == true.
         x_step = iter_x!(x, pre_matrix, P, c, A, y, y_prev, return_run_data)
         s_step = iter_s!(s, A, x, b, y, K, ρ, return_run_data)
         y_step = iter_y!(y, A, x, s, b, ρ, return_run_data)
+        concat_step[1:n] .= x_step
+        normalised_concat_step[1:n] .= dim_adjusted_vec_normalise(x_step)
+        concat_step[n+1:n+m] .= s_step
+        normalised_concat_step[n+1:n+m] .= dim_adjusted_vec_normalise(s_step)
+        concat_step[n+m+1:end] .= y_step
+        normalised_concat_step[n+m+1:end] .= dim_adjusted_vec_normalise(y_step)
 
         # Note: we skip the first iteration, when there is no "previous" step.
         if return_run_data && k >= 1
             # Question: split angles into steps in different variables or not?
             x_step_angle = acos(dot(x_step, x_step_prev) / (norm(x_step) * norm(x_step_prev)))
             s_step_angle = acos(dot(s_step, s_step_prev) / (norm(s_step) * norm(s_step_prev)))
+            # Avoid nans for zero steps, put in zeros instead.
+            s_step_angle = isnan(s_step_angle) ? 0.0 : s_step_angle
             y_step_angle = acos(dot(y_step, y_step_prev) / (norm(y_step) * norm(y_step_prev)))
+            concat_step_angle = acos(dot(concat_step, concat_step_prev) / (norm(concat_step) * norm(concat_step_prev)))
+            normalised_concat_step_angle = acos(dot(normalised_concat_step, normalised_concat_step_prev) / (norm(normalised_concat_step) * norm(normalised_concat_step_prev)))
             push!(x_step_angles, x_step_angle)
             push!(s_step_angles, s_step_angle)
             push!(y_step_angles, y_step_angle)
+            push!(concat_step_angles, concat_step_angle)
+            push!(normalised_concat_step_angles, normalised_concat_step_angle)
         end
 
         x_step_prev = isnothing(x_step) ? nothing : copy(x_step)
         s_step_prev = isnothing(s_step) ? nothing : copy(s_step)
         y_step_prev = isnothing(y_step) ? nothing : copy(y_step)
+        concat_step_prev = isnothing(concat_step) ? nothing : copy(concat_step)
+        normalised_concat_step_prev = isnothing(normalised_concat_step) ? nothing : copy(normalised_concat_step)
+            
 
 
         # Keep "previous" dual variable, used in the x update.
@@ -204,7 +234,7 @@ function optimise!(problem::QPProblem, variant::Integer, x::Vector{Float64},
     end
 
     if return_run_data
-        return primal_obj_vals, dual_obj_vals, primal_res_norms, dual_res_norms, x_step_angles, s_step_angles, y_step_angles
+        return primal_obj_vals, dual_obj_vals, primal_res_norms, dual_res_norms, x_step_angles, s_step_angles, y_step_angles, concat_step_angles, normalised_concat_step_angles
     else
         return primal_obj_val(P, c, x), dual_obj_val(P, b, x, y)
     end
