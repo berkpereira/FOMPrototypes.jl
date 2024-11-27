90c4692e763473b418b15bbbe63fef7e477a7507
Gabriel Pereira <berkpereira@gmail.com>
2024-11-27 16:20:29 +0000

    introduced first proposal of an adaptive restart rule based on cumulative update/step angles

--- src/solver.jl
+++ src/solver.jl
@@ -14,18 +14,13 @@ function iter_x!(x::AbstractVector{Float64},
     c::AbstractVector{Float64},
     A::AbstractMatrix{Float64},
     y::AbstractVector{Float64},
-    y_prev::AbstractVector{Float64},
-    return_step::Bool = false)
+    y_prev::AbstractVector{Float64})
 
     # NOTE: pre_matrix is stored inverted, hence just multiplication here.
     step = - pre_matrix * (P * x + c + A' * (2 * y - y_prev))
     x .+= step
-    
-    if return_step
-        return step
-    else
-        return nothing
-    end
+
+    return step
 end
 
 function iter_s!(s::AbstractVector{Float64},
@@ -34,12 +29,9 @@ function iter_s!(s::AbstractVector{Float64},
     b::AbstractVector{Float64},
     y::AbstractVector{Float64},
     K::Vector{Clarabel.SupportedCone},
-    ρ::Float64,
-    return_step::Bool = false)
+    ρ::Float64)
 
-    if return_step
-        s_old = copy(s)
-    end
+    s_old = copy(s)
 
     s .= b - A * x - y / ρ
 
@@ -58,12 +50,8 @@ function iter_s!(s::AbstractVector{Float64},
         
         start_idx = end_idx + 1
     end
-
-    if return_step
-        return s - s_old
-    else
-        return nothing
-    end
+    
+    return s - s_old
 end
 
 function iter_y!(y::AbstractVector{Float64},
@@ -71,15 +59,26 @@ function iter_y!(y::AbstractVector{Float64},
     x::AbstractVector{Float64},
     s::AbstractVector{Float64},
     b::AbstractVector{Float64},
-    ρ::Float64,
-    return_step::Bool = false)
-    if return_step
-        step = ρ * (A * x + s - b)
-        y .+= step
-        return step
+    ρ::Float64)
+    step = ρ * (A * x + s - b)
+    y .+= step
+    return step
+end
+
+"""
+When solver is run with (prototype) adaptive restart mechanism, this function 
+determines whether it is time for a restart or not, based on the cumulative
+angle data for each iterate sequence.
+"""
+function restart_trigger(restart_period::Union{Real, Symbol}, k::Integer,
+    cumsum_angles::Float64...)
+    if restart_period == Inf
+        return false
+    elseif restart_period == :adaptive
+        return all(v >= 2π for v in cumsum_angles)
+        # return any(v >= 2π for v in cumsum_angles)
     else
-        y .+= ρ * (A * x + s - b)
-        return nothing
+        return k > 0 && k % restart_period == 0
     end
 end
 
@@ -103,8 +102,11 @@ function optimise!(problem::QPProblem, variant::Integer, x::Vector{Float64},
     # Compute the (fixed) matrix that premultiplies the x step
     pre_matrix = pre_x_step_matrix(variant, P, A_gram, τ, ρ, n)
 
-    # keep old dual variable, convenient for x update.
+    # Keep old dual variable, convenient for x update.
     y_prev = copy(y)
+
+    # Initialise running sums of step angles.
+    x_angle_sum, s_angle_sum, y_angle_sum = 0.0, 0.0, 0.0
     
     # for restarts, keep average iterates
     if restart_period != Inf
@@ -118,34 +120,34 @@ function optimise!(problem::QPProblem, variant::Integer, x::Vector{Float64},
     dual_res_temp = zeros(Float64, n)
     j = 0
 
-    # Data containers for metrics (if return_run_data).
+    # Data containers for metrics (if return_run_data == true).
     if return_run_data
         x_step_angles = Float64[]
         s_step_angles = Float64[]
         y_step_angles = Float64[]
         concat_step_angles = Float64[]
-        # NOTE: this vector normalises the vectors
         normalised_concat_step_angles = Float64[]
         primal_obj_vals = Float64[]
         dual_obj_vals = Float64[]
         primal_res_norms = Float64[]
         dual_res_norms = Float64[]
-        x_step = zeros(Float64, n)
-        s_step = zeros(Float64, m)
-        y_step = zeros(Float64, m)
-        concat_step = zeros(Float64, n + 2 * m)
-        normalised_concat_step = zeros(Float64, n + 2 * m)
-        x_step_prev = zeros(Float64, n)
-        s_step_prev = zeros(Float64, m)
-        y_step_prev = zeros(Float64, m)
-        concat_step_prev = zeros(Float64, n + 2 * m)
-    else
-        x_step_prev = nothing
-        s_step_prev = nothing
-        y_step_prev = nothing
-        concat_step_prev = nothing
-        normalised_concat_step_prev = nothing
     end
+
+    # Initialise other variables to be used during algorithm.
+    x_step = zeros(Float64, n)
+    s_step = zeros(Float64, m)
+    y_step = zeros(Float64, m)
+    concat_step = zeros(Float64, n + 2 * m)
+    normalised_concat_step = zeros(Float64, n + 2 * m)
+    x_step_prev = zeros(Float64, n)
+    s_step_prev = zeros(Float64, m)
+    y_step_prev = zeros(Float64, m)
+    concat_step_prev = zeros(Float64, n + 2 * m)
+    normalised_concat_step_prev = zeros(Float64, n + 2 * m)
+
+    # This flag helps us keep track of whether we should forgo the notion of a
+    # "previous" step, including in the very first iteration.
+    just_restarted = true
 
     for k in 0:max_iter
         # Update average iterates
@@ -153,18 +155,22 @@ function optimise!(problem::QPProblem, variant::Integer, x::Vector{Float64},
             x_avg .= (j * x_avg + x) / (j + 1)
             s_avg .= (j * s_avg + s) / (j + 1)
             y_avg .= (j * y_avg + y) / (j + 1)
-            if k > 0 && k % restart_period == 0
-                # restart
+            if restart_trigger(restart_period, k, x_angle_sum, s_angle_sum, y_angle_sum)
+                # Restart.
                 x .= x_avg
                 s .= s_avg
                 y .= y_avg
 
-                # reset averages
+                # Reset iterate averages.
                 x_avg .= x
                 s_avg .= s
                 y_avg .= y
 
+                # Reset inner loop counters/sums.
                 j = 0
+                x_angle_sum, s_angle_sum, y_angle_sum = 0.0, 0.0, 0.0
+
+                just_restarted = true
             end
         end
 
@@ -191,10 +197,10 @@ function optimise!(problem::QPProblem, variant::Integer, x::Vector{Float64},
             push!(dual_res_norms, curr_dual_res_norm)
         end
 
-        # Iterate and record step vectors if return_run_data == true.
-        x_step = iter_x!(x, pre_matrix, P, c, A, y, y_prev, return_run_data)
-        s_step = iter_s!(s, A, x, b, y, K, ρ, return_run_data)
-        y_step = iter_y!(y, A, x, s, b, ρ, return_run_data)
+        # Iterate and record step vectors.
+        x_step = iter_x!(x, pre_matrix, P, c, A, y, y_prev)
+        s_step = iter_s!(s, A, x, b, y, K, ρ)
+        y_step = iter_y!(y, A, x, s, b, ρ)
         concat_step[1:n] .= x_step
         normalised_concat_step[1:n] .= dim_adjusted_vec_normalise(x_step)
         concat_step[n+1:n+m] .= s_step
@@ -202,34 +208,44 @@ function optimise!(problem::QPProblem, variant::Integer, x::Vector{Float64},
         concat_step[n+m+1:end] .= y_step
         normalised_concat_step[n+m+1:end] .= dim_adjusted_vec_normalise(y_step)
 
-        # Note: we skip the first iteration, when there is no "previous" step.
-        if return_run_data && k >= 1
-            # Question: split angles into steps in different variables or not?
+        # NOTE: we skip the iterations just after a restart (or the first one).
+        if !just_restarted
             x_step_angle = acos(dot(x_step, x_step_prev) / (norm(x_step) * norm(x_step_prev)))
+            x_angle_sum += x_step_angle
             s_step_angle = acos(dot(s_step, s_step_prev) / (norm(s_step) * norm(s_step_prev)))
-            # Avoid nans for zero steps, put in zeros instead.
+            # Avoid nans for zero steps: put in zeros instead.
+            # NOTE: may want to replicate this for x and y steps too.
             s_step_angle = isnan(s_step_angle) ? 0.0 : s_step_angle
+            s_angle_sum += s_step_angle
             y_step_angle = acos(dot(y_step, y_step_prev) / (norm(y_step) * norm(y_step_prev)))
-            concat_step_angle = acos(dot(concat_step, concat_step_prev) / (norm(concat_step) * norm(concat_step_prev)))
-            normalised_concat_step_angle = acos(dot(normalised_concat_step, normalised_concat_step_prev) / (norm(normalised_concat_step) * norm(normalised_concat_step_prev)))
-            push!(x_step_angles, x_step_angle)
-            push!(s_step_angles, s_step_angle)
-            push!(y_step_angles, y_step_angle)
-            push!(concat_step_angles, concat_step_angle)
-            push!(normalised_concat_step_angles, normalised_concat_step_angle)
+            y_angle_sum += y_step_angle
+            
+            # NOTE: this data has indexing "gaps" corresponding to when a
+            # restart occurs, since it doesn't make sense to include those as
+            # angles between consecutive steps in the usual sense.
+            if return_run_data
+                concat_step_angle = acos(dot(concat_step, concat_step_prev) / (norm(concat_step) * norm(concat_step_prev)))
+                normalised_concat_step_angle = acos(dot(normalised_concat_step, normalised_concat_step_prev) / (norm(normalised_concat_step) * norm(normalised_concat_step_prev)))
+                push!(x_step_angles, x_step_angle)
+                push!(s_step_angles, s_step_angle)
+                push!(y_step_angles, y_step_angle)
+                push!(concat_step_angles, concat_step_angle)
+                push!(normalised_concat_step_angles, normalised_concat_step_angle)
+            end
         end
 
-        x_step_prev = isnothing(x_step) ? nothing : copy(x_step)
-        s_step_prev = isnothing(s_step) ? nothing : copy(s_step)
-        y_step_prev = isnothing(y_step) ? nothing : copy(y_step)
-        concat_step_prev = isnothing(concat_step) ? nothing : copy(concat_step)
-        normalised_concat_step_prev = isnothing(normalised_concat_step) ? nothing : copy(normalised_concat_step)
-            
+        x_step_prev = copy(x_step)
+        s_step_prev = copy(s_step)
+        y_step_prev = copy(y_step)
+        concat_step_prev = copy(concat_step)
+        normalised_concat_step_prev = copy(normalised_concat_step)
 
+        # Notion of a previous iterate step makes sense (now).
+        just_restarted = false
 
         # Keep "previous" dual variable, used in the x update.
-        y_prev = y
-
+        y_prev = copy(y)
+        
         j += 1
     end
 
