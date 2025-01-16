5af1b9a6172c51a4d896e1504e5405bd1af14a01
Gabriel Pereira <berkpereira@gmail.com>
2025-01-16 13:32:18 +0000

    now include line search in either (x, v) or in (x, s, y) spaces, for testing

--- src/linesearch.jl
+++ src/linesearch.jl
@@ -26,6 +26,16 @@ function fixed_point_linesearch!(ws::Workspace, α_max::Float64,
     β::Float64, ϵ::Float64, krylov_operator_tilde_A::Bool)
     @assert α_max > 1.0
 
+    # I'm still experimenting with proof of concept, so I implement two
+    # versions of this function in parallel.
+    # One of them interprets lines in (x, v) space, and the other 
+    # interprets lines in (x, s, y) space.
+    # I think the latter is correct for the Giselsson et al method, but still
+    # good to check practical performance, since I can.
+    
+    # CHOOSE:
+    LINES = :xv # in {:xv, :xsy}
+
     # Compute (would-be) vanilla iterate from the current (x, v)_k and
     # Arnoldi "q vector", which we access from ws.vars.x_v_q.
     # We take the opportunity to also take an Arnoldi process step here,
@@ -42,92 +52,113 @@ function fixed_point_linesearch!(ws::Workspace, α_max::Float64,
     # Compute the current iterate's fixed-point residual.
     # We may need to "translate" to (x, s, y) space for the line search.
     # In production code this will be handled much more gracefully...
-    curr_iterate_s = project_to_K(ws.vars.x_v_q[ws.p.n+1:end, 1], ws.p.K)
-    curr_iterate_y = ws.ρ * (curr_iterate_s - ws.vars.x_v_q[ws.p.n+1:end, 1])
-    curr_iterate_xsy = [ws.vars.x_v_q[1:ws.p.n, 1]; curr_iterate_s; curr_iterate_y]
+    if LINES == :xv
+        curr_fp_residual = vanilla_iterate_x_v_q[:, 1] - ws.vars.x_v_q[:, 1]
 
-    vanilla_iterate_s = project_to_K(vanilla_iterate_x_v_q[ws.p.n+1:end, 1], ws.p.K)
-    vanilla_iterate_y = ws.ρ * (vanilla_iterate_s - vanilla_iterate_x_v_q[ws.p.n+1:end, 1])
-    vanilla_iterate_xsy = [vanilla_iterate_x_v_q[1:ws.p.n, 1]; vanilla_iterate_s; vanilla_iterate_y]
-    
-    # curr_fp_residual = vanilla_iterate_x_v_q[:, 1] - ws.vars.x_v_q[:, 1]
-    curr_fp_residual = vanilla_iterate_xsy - curr_iterate_xsy
+        # To comute fixed-point residual at the vanilla iterate, first
+        # have to determine the local (x, v) dynamics at the vanilla point.
+        vanilla_iterate_s = project_to_K(vanilla_iterate_x_v_q[ws.p.n+1:end, 1], ws.p.K)
+        vanilla_enforced_constraints = enforced_contraints_bitvec(ws, vanilla_iterate_x_v_q[ws.p.n+1:end, 1], vanilla_iterate_s)
 
-    # To determine the FP residual at the vanilla iterate, must first determine
-    # the local affine dynamics of the method.
-    # Recall that s = proj_K(v).
-    
-    # vanilla_iterate_s = project_to_K(ws.vars.x_v_q[ws.p.n+1:end, 1], ws.p.K)
-    vanilla_enforced_constraints = enforced_contraints_bitvec(ws, vanilla_iterate_x_v_q[ws.p.n+1:end, 1], vanilla_iterate_s)
+        # Compute T(vanilla iterate), where T is the FOM operator.
+        # TODO: include UPDATE OF tilde_b to reflect the dynamics at the vanilla
+        # iterate! In the special case of a QP it is invariant, but NOT in general!
+        vanilla_lookahead = tilde_A_prod(ws, vanilla_enforced_constraints, vanilla_iterate_x_v_q[:, 1]) + ws.tilde_b
 
-    # Compute T(vanilla iterate), where T is the FOM operator.
-    # TODO: include UPDATE OF tilde_b to reflect the dynamics at the vanilla
-    # iterate! In the special case of a QP it is invariant, but NOT in general!
-    vanilla_lookahead = tilde_A_prod(ws, vanilla_enforced_constraints, vanilla_iterate_x_v_q[:, 1]) + ws.tilde_b
+        # Now compute fixed-point residual at vanilla iterate.
+        vanilla_fp_merit = norm(vanilla_lookahead - vanilla_iterate_x_v_q[:, 1])
 
-    # Again translate to (x, s, y) space.
-    vanilla_lookahead_s = project_to_K(vanilla_lookahead[ws.p.n+1:end], ws.p.K)
-    vanilla_lookahead_y = ws.ρ * (vanilla_lookahead_s - vanilla_lookahead[ws.p.n+1:end])
-    vanilla_lookahead_xsy = [vanilla_lookahead[1:ws.p.n]; vanilla_lookahead_s; vanilla_lookahead_y]
+        candidate = similar(vanilla_lookahead)
+        candidate_s = similar(vanilla_iterate_s)
+        candidate_lookahead = similar(vanilla_lookahead)
+        candidate_enforced_constraints = similar(vanilla_enforced_constraints)
+    elseif LINES == :xsy
+        curr_iterate_s = project_to_K(ws.vars.x_v_q[ws.p.n+1:end, 1], ws.p.K)
+        curr_iterate_y = ws.ρ * (curr_iterate_s - ws.vars.x_v_q[ws.p.n+1:end, 1])
+        curr_iterate_xsy = [ws.vars.x_v_q[1:ws.p.n, 1]; curr_iterate_s; curr_iterate_y]
 
-    # Compute fixed-point residual NORM at the vanilla iterate.
-    # vanilla_fp_merit = norm(vanilla_lookahead - vanilla_iterate_x_v_q[:, 1])
-    vanilla_fp_merit = norm(vanilla_lookahead_xsy - vanilla_iterate_xsy)
+        vanilla_iterate_s = project_to_K(vanilla_iterate_x_v_q[ws.p.n+1:end, 1], ws.p.K)
+        vanilla_iterate_y = ws.ρ * (vanilla_iterate_s - vanilla_iterate_x_v_q[ws.p.n+1:end, 1])
+        vanilla_iterate_xsy = [vanilla_iterate_x_v_q[1:ws.p.n, 1]; vanilla_iterate_s; vanilla_iterate_y]
+        
+        curr_fp_residual = vanilla_iterate_xsy - curr_iterate_xsy
 
-    # Now move on to the line search proper.
+        # Now need fixed-point residual AT the vanilla point.
+        vanilla_lookahead_x = iter_x(vanilla_iterate_xsy[1:ws.p.n], ws.cache[:W_inv], ws.p.P, ws.p.c, ws.p.A, vanilla_iterate_y, curr_iterate_y)
+        vanilla_lookahead_s = iter_s(vanilla_iterate_s, ws.p.A, vanilla_lookahead_x, ws.p.b, vanilla_iterate_y, ws.p.K, ws.ρ)
+        vanilla_lookahead_y = iter_y(vanilla_iterate_y, ws.p.A, vanilla_lookahead_x, vanilla_lookahead_s, ws.p.b, ws.ρ)
+        
+        vanilla_lookahead = [vanilla_lookahead_x; vanilla_lookahead_s; vanilla_lookahead_y]
+
+        # Now compute fixed-point residual at vanilla iterate.
+        vanilla_fp_merit = norm(vanilla_lookahead - vanilla_iterate_xsy)
+
+        candidate = similar(vanilla_lookahead)
+        candidate_lookahead = similar(vanilla_lookahead)
+    end
+
+    # Now move on to the actual line search.
     # Note that the search direction is simply the current fp residual.
     α = α_max
-    # candidate = similar(ws.vars.x_v_q[:, 1]) # pre-allocate.
-    candidate = similar(curr_iterate_xsy)
-    
-    candidate_s = similar(vanilla_iterate_s)
-    candidate_enforced_constraints = similar(vanilla_enforced_constraints)
-    candidate_lookahead = similar(vanilla_lookahead)
     
     candidate_fp_merit = 0.0
     while α > 1.0 # Backtrack until we hit the nominal step length.
-        
-        # candidate .= ws.vars.x_v_q[:, 1] + α * curr_fp_residual
-        
-        candidate .= curr_iterate_xsy + α * curr_fp_residual
-
-        # Recover v.
-        candidate_v = candidate[ws.p.n+1:ws.p.n+ws.p.m] - candidate[ws.p.n+ws.p.m+1:end] / ws.ρ
-
-        # Merit function is norm of fp residual, so we repeat a lot of the
-        # steps we went through in the code above.
-        # candidate_s .= project_to_K(candidate[ws.p.n+1:end], ws.p.K)
-        # candidate_enforced_constraints .= enforced_contraints_bitvec(ws, candidate[ws.p.n+1:end], candidate_s)
-        candidate_enforced_constraints .= enforced_contraints_bitvec(ws, candidate_v, candidate[ws.p.n+1:ws.p.n+ws.p.m])
-        candidate_lookahead .= tilde_A_prod(ws, candidate_enforced_constraints, [candidate[1:ws.p.n]; candidate_v]) + ws.tilde_b
-        
-        # candidate_fp_merit = norm(candidate_lookahead - candidate)
-        
-        # Translate to (x, s, y) space AGAIN.
-        candidate_lookahead_s = project_to_K(candidate_lookahead[ws.p.n+1:end], ws.p.K)
-        candidate_lookahead_y = ws.ρ * (candidate_lookahead_s - candidate_lookahead[ws.p.n+1:end])
-        candidate_lookahead_xsy = [candidate_lookahead[1:ws.p.n]; candidate_lookahead_s; candidate_lookahead_y]
-        candidate_fp_merit = norm(candidate_lookahead_xsy - candidate)
-
-
-        # Acceptance criterion is based on reduction of the merit function
-        # as compared to the vanilla iterate, by factor (1 - ϵ).
-        if candidate_fp_merit <= (1 - ϵ) * vanilla_fp_merit
-            println("Successful line search with α = $α.")
-            println("Successful Candidate merit: $candidate_fp_merit")
-            println("Vanilla merit: $vanilla_fp_merit")
+        if LINES == :xv
+            candidate .= ws.vars.x_v_q[:, 1] + α * curr_fp_residual
             
-            # Translate back to (x, v) space AGAIN...
-            # ws.vars.x_v_q[:, 1] .= candidate
-            ws.vars.x_v_q[:, 1] .= [candidate[1:ws.p.n]; candidate_v]
-            return true
-        end
+            # First determine the local method dynamics AT the candidate.
+            candidate_s .= project_to_K(candidate[ws.p.n+1:end], ws.p.K)
+            candidate_enforced_constraints .= enforced_contraints_bitvec(ws, candidate[ws.p.n+1:end], candidate_s)
+            
+            # Compute lookahead iterate FROM the candidate.
+            # TODO: include UPDATE OF tilde_b to reflect the dynamics at the candidate point. This is ONLY constant in QP special case!
+            candidate_lookahead .= tilde_A_prod(ws, candidate_enforced_constraints, candidate) + ws.tilde_b
 
-        # Backtrack by factor β.
-        α *= β
+            # Hence compute fixed-point residual norm at candidate.
+            candidate_fp_merit = norm(candidate_lookahead - candidate)
+
+            if candidate_fp_merit <= (1 - ϵ) * vanilla_fp_merit
+                println("Successful line search with α = $α.")
+                println("Successful Candidate merit: $candidate_fp_merit")
+                println("Vanilla merit: $vanilla_fp_merit")
+                
+                # Assign to the workspace variable.
+                ws.vars.x_v_q[:, 1] .= candidate
+                return true
+            end
+
+            # Backtrack by factor β.
+            α *= β
+        elseif LINES == :xsy
+            candidate .= curr_iterate_xsy + α * curr_fp_residual
+
+            # Compute lookahead iterate FROM the candidate.
+            candidate_lookahead[1:ws.p.n] .= iter_x(candidate[1:ws.p.n], ws.cache[:W_inv], ws.p.P, ws.p.c, ws.p.A, candidate[ws.p.n+ws.p.m+1:end], candidate[ws.p.n+ws.p.m+1:end] - ws.ρ * (ws.p.A * candidate[1:ws.p.n] + candidate[ws.p.n+1:ws.p.n+ws.p.m] - ws.p.b))
+            candidate_lookahead[ws.p.n+1:ws.p.n+ws.p.m] .= iter_s(candidate[ws.p.n+1:ws.p.n+ws.p.m], ws.p.A, candidate_lookahead[1:ws.p.n], ws.p.b, candidate[ws.p.n+ws.p.m+1:end], ws.p.K, ws.ρ)
+            candidate_lookahead[ws.p.n+ws.p.m+1:end] .= iter_y(candidate[ws.p.n+ws.p.m+1:end], ws.p.A, candidate_lookahead[1:ws.p.n], candidate_lookahead[ws.p.n+1:ws.p.n+ws.p.m], ws.p.b, ws.ρ)
+
+            # Hence compute fixed-point residual norm at candidate.
+            candidate_fp_merit = norm(candidate_lookahead - candidate)
+
+            if candidate_fp_merit <= (1 - ϵ) * vanilla_fp_merit
+                println("Successful line search with α = $α.")
+                println("Successful Candidate merit: $candidate_fp_merit")
+                println("Vanilla merit: $vanilla_fp_merit")
+                
+                candidate_v = candidate[ws.p.n+1:ws.p.m+ws.p.n] - candidate[ws.p.m+ws.p.n+1:end] / ws.ρ
+
+                # Assign to the workspace variable.
+                ws.vars.x_v_q[:, 1] .= [candidate[1:ws.p.n]; candidate_v]
+                return true
+            end
+
+            # Backtrack by factor β.
+            α *= β
+        end
     end
-    println("Failed linesearch. Last at α = $(α / β), candidate/vanilla residual: $(candidate_fp_merit / vanilla_fp_merit)")
     
+    println("Failed linesearch. Last at α = $(α / β), candidate/vanilla residual: $(candidate_fp_merit / vanilla_fp_merit)")
+
     # If we reach here, we just assign the vanilla iterate.
     ws.vars.x_v_q[:, 1] .= vanilla_iterate_x_v_q[:, 1]
     return false
