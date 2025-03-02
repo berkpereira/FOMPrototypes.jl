db57295aee52357961ccd41d9ab7b56b346c57d8
Gabriel Pereira <berkpereira@gmail.com>
2025-03-02 19:00:42 +0000

    add find_problems.jl to filter and save suitable problems based on search criteria

---
+++ src/find_problems.jl
@@ -0,0 +1,27 @@
+include("problem_data.jl")
+
+search_problem_set = "opf_lp" # in {"sslsq", "maros", "opf_lp"}
+
+# Define search criteria
+MIN_M = 1
+MAX_M = 3000
+MIN_N = 10
+MAX_N = 3000
+
+suitable_problems = filter_clarabel_problems(search_problem_set,
+    min_m = MIN_M, max_m = MAX_M, min_n = MIN_N, max_n = MAX_N)
+
+# Write results to a text file including an info header line.
+names_file = "./search_results_$search_problem_set.txt"
+open(names_file, "w") do f
+    # Write header with search criteria
+    header = "#SEARCH{set:$search_problem_set,min_m:$MIN_M,max_m:$MAX_M,min_n:$MIN_N,max_n:$MAX_N}"
+    println(f, header)
+    
+    # Write problem names
+    for problem in suitable_problems
+        println(f, problem)
+    end
+end
+
+retrieved_problems = readlines(names_file)
