60ab0c6b22d66560048fc5a95431bc13cc1b7ea9
Gabriel Pereira <berkpereira@gmail.com>
2025-05-07 16:14:25 +0100

    small fixes

--- src/FOMPrototypes.jl
+++ src/FOMPrototypes.jl
@@ -2,6 +2,18 @@ module FOMPrototypes
 
 export main, run_cli, run_prototype, solve_reference, fetch_data
 
+# Import packages.
+using ArgParse
+using Infiltrator
+using LinearMaps
+using Infiltrator
+using Profile
+using BenchmarkTools
+using Plots
+using SparseArrays
+using SCS
+using Random
+
 # Include all project source files.
 include(joinpath(@__DIR__, "custom_nla.jl"))
 include(joinpath(@__DIR__, "types.jl"))
@@ -12,17 +24,6 @@ include(joinpath(@__DIR__, "linesearch.jl"))
 include(joinpath(@__DIR__, "printing.jl"))
 include(joinpath(@__DIR__, "solver.jl"))
 include(joinpath(@__DIR__, "problem_data.jl"))
-
-# Import packages.
-using ArgParse
-using LinearMaps
-using Infiltrator
-using Profile
-using BenchmarkTools
-using Plots
-using SparseArrays
-using SCS
-using Random
 
 ########################
 # Initialization Block #
@@ -115,6 +116,21 @@ function parse_command_line()
         help = "Period for Anderson acceleration."
         arg_type = Int
         default = 10
+
+        "--anderson-broyden-type"
+        help = "Which type of Broyden update to use: 1, normal2, or QR2."
+        arg_type = Symbol
+        default = :normal2
+
+        "--anderson-mem-type"
+        help = "Memory type for Anderson acceleration: rolling or restarted."
+        arg_type = Symbol
+        default = :rolling
+
+        "--anderson-reg"
+        help = "Regulariser for Anderson least-squares problem: none, Tikonov, or Frobenius."
+        arg_type = Symbol
+        default = :none
 
         "--run-fast"
         help = "Run fast mode (no plotting, less data recording during run)."
@@ -290,11 +306,11 @@ function run_prototype(problem::ProblemData, args::Dict{String, Any};
 
     # initialise the workspace
     if args["acceleration"] == :krylov
-        ws = KrylovWorkspace{Float64}(problem, args["variant"], A_gram, τ, args["rho"], args["theta"], args["accel-memory"], args["krylov-operator"])
+        ws = KrylovWorkspace(problem, args["variant"], τ, args["rho"], args["theta"], args["accel-memory"], args["krylov-operator"], A_gram = A_gram)
     elseif args["acceleration"] == :anderson
-        ws = AndersonWorkspace(problem, args["variant"], A_gram, τ, args["rho"], args["theta"], args["accel-memory"], args["anderson-period"])
+        ws = AndersonWorkspace(problem, args["variant"], τ, args["rho"], args["theta"], args["accel-memory"], args["anderson-period"], A_gram = A_gram, broyden_type = args["anderson-broyden-type"], memory_type = args["anderson-mem-type"], regulariser_type = args["anderson-reg"])
     else
-        ws = NoneWorkspace(problem, args["variant"], A_gram, τ, args["rho"], args["theta"])
+        ws = NoneWorkspace(problem, args["variant"], τ, args["rho"], args["theta"], A_gram = A_gram)
     end
 
     # Run the solver (time or profile execution)
@@ -319,29 +335,36 @@ end
 # Refactored Plotting Block #
 #############################
 
-function plot_results(results, variant, max_iter, restart_period, acceleration,
-    accel_memory, linesearch_period, newline_char,
-    problem_set, problem_name, krylov_operator; show_vlines::Bool = true)
+function plot_results(results, args, newline_char)
+
+    k_final = length(results.data[:primal_obj_vals])
     
     # plotting constants
     LINEWIDTH = 2.5
     VERT_LINEWIDTH = 1.5
     ALPHA = 0.9
 
-    title_beginning = "Problem: $problem_set $problem_name.$newline_char Variant $variant $newline_char"
-    title_end = "$newline_char Restart period = $restart_period.$newline_char Acceleration: $acceleration (memory = period = $accel_memory).$newline_char Linesearch period = $linesearch_period."
-    
-    if krylov_operator == :tilde_A
-        krylov_operator_str = "$newline_char Krylov operator is A"
-    else
-        krylov_operator_str = "$newline_char Krylov operator is B = A – I"
+    # Common title components
+    title_common = "Problem: $(args["problem-set"]) $(args["problem-name"]).$newline_char Variant $(args["variant"]) $newline_char"
+    title_common *= "Restart period = $(args["restart-period"]).$newline_char Linesearch period = $(args["linesearch-period"])$newline_char"
+    if args["acceleration"] == :none
+        title_common *= "Acceleration: none.$newline_char"
+        krylov_operator_str = ""
+    elseif args["acceleration"] == :anderson
+        title_common *= "Anderson acceleration: mem = $(args["accel-memory"]), period = $(args["anderson-period"]),$newline_char broyden = $(args["anderson-broyden-type"]), mem_type = $(args["anderson-mem-type"]).$newline_char"
+        krylov_operator_str = ""
+    elseif args["acceleration"] == :krylov
+        title_common *= "Krylov acceleration: mem = $(args["accel-memory"]), op = $(args["krylov-operator"]).$newline_char"
     end
 
+    # Add Krylov operator string if acceleration is :krylov
+    
+    
     constraint_lines = constraint_changes(results.data[:record_proj_flags])
 
     # Helper function to add common vertical lines, only if show_vlines is true.
     function add_vlines!(plt; constraint_style=(:dash, ALPHA, :green, VERT_LINEWIDTH))
-        if show_vlines
+        if args["show-vlines"]
             vline!(plt, results.data[:acc_step_iters], line = (:dash, ALPHA, :red, VERT_LINEWIDTH), label="Accelerated Steps")
             vline!(plt, results.data[:linesearch_iters], line = (:dash, ALPHA, :maroon, VERT_LINEWIDTH), label="Line Search Steps")
             vline!(plt, constraint_lines, line = constraint_style, label="Active set changes")
@@ -350,73 +373,73 @@ function plot_results(results, variant, max_iter, restart_period, acceleration,
     end
 
     # Primal objective plot.
-    primal_obj_plot = plot(0:max_iter+1, results.data[:primal_obj_vals], linewidth=LINEWIDTH,
+    primal_obj_plot = plot(0:k_final-1, results.data[:primal_obj_vals], linewidth=LINEWIDTH,
     label="Prototype Objective", xlabel="Iteration", ylabel="Objective Value",
-    title="$title_beginning Objective $krylov_operator_str $title_end")
+    title="$title_common Objective")
     add_vlines!(primal_obj_plot)
     display(primal_obj_plot)
 
     # Dual objective plot.
-    dual_obj_plot = plot(0:max_iter+1, results.data[:dual_obj_vals], linewidth=LINEWIDTH,
+    dual_obj_plot = plot(0:k_final-1, results.data[:dual_obj_vals], linewidth=LINEWIDTH,
     label="Prototype Dual Objective", xlabel="Iteration", ylabel="Dual Objective Value",
-    title="$title_beginning Dual objective $krylov_operator_str $title_end")
+    title="$title_common Dual Objective")
     add_vlines!(dual_obj_plot)
     display(dual_obj_plot)
 
     # Duality gap plot.
-    gap_plot = plot(0:max_iter+1, results.data[:primal_obj_vals] - results.data[:dual_obj_vals], linewidth=LINEWIDTH,
+    gap_plot = plot(0:k_final-1, results.data[:primal_obj_vals] - results.data[:dual_obj_vals], linewidth=LINEWIDTH,
     label="Prototype Dual Objective", xlabel="Iteration", ylabel="Duality Gap",
-    title="$title_beginning Duality Gap $krylov_operator_str $title_end")
+    title="$title_common Duality Gap")
     add_vlines!(gap_plot)
     display(gap_plot)
 
     # Primal residual plot.
-    pres_plot = plot(0:max_iter+1, results.data[:pri_res_norms], linewidth=LINEWIDTH,
+    pres_plot = plot(0:k_final-1, results.data[:pri_res_norms], linewidth=LINEWIDTH,
     label="Prototype Residual", xlabel="Iteration", ylabel="Primal Residual",
-    title="$title_beginning Primal Residual Norm $krylov_operator_str $title_end", yaxis=:log)
+    title="$title_common Primal Residual Norm", yaxis=:log)
     add_vlines!(pres_plot)
     display(pres_plot)
 
     # Dual residual plot.
-    dres_plot = plot(0:max_iter+1, results.data[:dual_res_norms], linewidth=LINEWIDTH,
+    dres_plot = plot(0:k_final-1, results.data[:dual_res_norms], linewidth=LINEWIDTH,
     label="Prototype Dual Residual", xlabel="Iteration", ylabel="Dual Residual",
-    title="$title_beginning Dual Residual Norm $krylov_operator_str $title_end", yaxis=:log)
+    title="$title_common Dual Residual Norm", yaxis=:log)
     add_vlines!(dres_plot)
     display(dres_plot)
 
     # (x, y) distance to solution plot.
     xy_dist_to_sol = sqrt.(results.data[:x_dist_to_sol] .^ 2 .+ results.data[:y_dist_to_sol] .^ 2)
-    xy_dist_plot = plot(0:max_iter+1, xy_dist_to_sol, linewidth=LINEWIDTH,
+    xy_dist_plot = plot(0:k_final, xy_dist_to_sol, linewidth=LINEWIDTH,
         label="Prototype (x, y) Distance", xlabel="Iteration", ylabel="Distance to Solution",
-        title="$title_beginning (x, y) Distance to Solution $krylov_operator_str $title_end", yaxis=:log)
+        title="$title_common (x, y) Distance to Solution", yaxis=:log)
     add_vlines!(xy_dist_plot)
     display(xy_dist_plot)
 
     # (x, y) characteristic norm distance to solution plot.
-    seminorm_plot = plot(0:max_iter+1, results.data[:xy_chardist], linewidth=LINEWIDTH,
+    seminorm_plot = plot(0:k_final, results.data[:xy_chardist], linewidth=LINEWIDTH,
     label="(x, y) Seminorm Distance (Theory)", xlabel="Iteration", ylabel="Distance to Solution",
-    title="$title_beginning (x, y) Characteristic Norm Distance to Solution $krylov_operator_str $title_end", yaxis=:log)
+    title="$title_common (x, y) Characteristic Norm Distance to Solution", yaxis=:log)
     add_vlines!(seminorm_plot)
     display(seminorm_plot)
 
     # (x, y) step norms plot.
-    xy_step_norms_plot = plot(0:max_iter, results.data[:xy_step_norms], linewidth=LINEWIDTH,
+    xy_step_norms_plot = plot(0:k_final-1, results.data[:xy_step_norms], linewidth=LINEWIDTH,
         label="(x, y) Step l2 Norm", xlabel="Iteration", ylabel="Step Norm",
-        title="$title_beginning (x, y) l2 Step Norm $krylov_operator_str $title_end", yaxis=:log)
+        title="$title_common (x, y) l2 Step Norm", yaxis=:log)
     add_vlines!(xy_step_norms_plot)
     display(xy_step_norms_plot)
 
     # (x, y) step CHAR norms plot.
-    xy_step_char_norms_plot = plot(0:max_iter, results.data[:xy_step_char_norms], linewidth=LINEWIDTH,
+    xy_step_char_norms_plot = plot(0:k_final-1, results.data[:xy_step_char_norms], linewidth=LINEWIDTH,
         label="(x, y) Step Char Norm", xlabel="Iteration", ylabel="Step CHAR Norm",
-        title="$title_beginning (x, y) CHAR Step Norm $krylov_operator_str $title_end", yaxis=:log)
+        title="$title_common (x, y) CHAR Step Norm", yaxis=:log)
     add_vlines!(xy_step_char_norms_plot)
     display(xy_step_char_norms_plot)
 
     # # Singular values ratio plot.
     # sing_vals_ratio_plot = plot(results.data[:update_mat_iters], results.data[:update_mat_singval_ratios], linewidth=LINEWIDTH,
     # label="Prototype Update Matrix", xlabel="Iteration", ylabel="First Two Singular Values' Ratio",
-    # title="$title_beginning Update Matrix Singular Value Ratio $krylov_operator_str $title_end",
+    # title="$title_beginning Update Matrix Singular Value Ratio  $title_end",
     # yaxis=:log, marker=:circle)
     # add_vlines!(sing_vals_ratio_plot)
     # display(sing_vals_ratio_plot)
@@ -424,7 +447,7 @@ function plot_results(results, variant, max_iter, restart_period, acceleration,
     # # Update matrix rank plot.
     # update_ranks_plot = plot(results.data[:update_mat_iters], results.data[:update_mat_ranks],
     # label="Prototype Update Matrix", xlabel="Iteration", ylabel="Rank",
-    # title="$title_beginning Update Matrix Rank $krylov_operator_str $title_end",
+    # title="$title_beginning Update Matrix Rank  $title_end",
     # linewidth=LINEWIDTH, xticks=0:100:MAX_ITER)
     # add_vlines!(update_ranks_plot)
     # display(update_ranks_plot)
@@ -433,9 +456,9 @@ function plot_results(results, variant, max_iter, restart_period, acceleration,
     # enforced_constraints_plot(results.data[:record_proj_flags])
 
     # Consecutive update (x, y) cosines plot.
-    xy_update_cosines_plot = plot(1:max_iter, results.data[:xy_update_cosines], linewidth=LINEWIDTH,
+    xy_update_cosines_plot = plot(1:k_final-1, results.data[:xy_update_cosines], linewidth=LINEWIDTH,
         label="Prototype Update Cosine", xlabel="Iteration", ylabel="Cosine of Consecutive Updates",
-        title="$title_beginning Consecutive (x, y) Update Cosines $krylov_operator_str $title_end")
+        title="$title_common Consecutive (x, y) Update Cosines")
     add_vlines!(xy_update_cosines_plot, constraint_style = (:dashdot, ALPHA, :green, VERT_LINEWIDTH))
     display(xy_update_cosines_plot)
 end
@@ -462,8 +485,6 @@ end
 ###########################
 
 function main(config::Dict{String, Any})
-    newline_char = initialise_misc()
-
     # Choose the problem and fetch data.
     println()
     println("About to import problem data...")
@@ -481,12 +502,11 @@ function main(config::Dict{String, Any})
     @time ws, results = run_prototype(problem, config, x_ref = x_ref, y_ref = y_ref)
 
     if !config["run-fast"]
+        newline_char = initialise_misc()
         println()
         println("About to plot results...")
-        plot_results(results, config["variant"], config["max-iter"], config["restart-period"], config["acceleration"],
-                    config["accel-memory"], config["linesearch-period"], newline_char,
-                    config["problem-set"], config["problem-name"], config["krylov-operator"],
-                    show_vlines = config["show-vlines"])
+        # plot_results(results, config["variant"], config["restart-period"], config["acceleration"], config["accel-memory"], config["linesearch-period"], newline_char, config["problem-set"], config["problem-name"], config["krylov-operator"], show_vlines = config["show-vlines"])
+        plot_results(results, config, newline_char)
     end
     
     #return data of interest to inspect
