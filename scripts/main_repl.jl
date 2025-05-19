e99c4d489edccf1e31cfdf43de9aaa5f0e815e01
Gabriel Pereira <berkpereira@gmail.com>
2025-05-19 12:18:07 +0100

    small changes

--- scripts/main_repl.jl
+++ scripts/main_repl.jl
@@ -2,20 +2,21 @@ using Revise
 import FOMPrototypes
 
 config = Dict(
-    "ref-solver"   => :SCS,
-    "variant"      => :ADMM, # in {:PDHG, :ADMM, Symbol(1), Symbol(2), Symbol(3), Symbol(4)}
+    "ref-solver"   => :Clarabel,
+    "variant"      => :PDHG, # in {:PDHG, :ADMM, Symbol(1), Symbol(2), Symbol(3), Symbol(4)}
     "problem-set"  => "sslsq",
-    "problem-name" => "NYPA_Maragal_5_lasso",
+    "problem-name" => "HB_abb313_lasso", # with PDHG, Krylov accelerated gets stuck even though unaccelerated works well...?
+    # "problem-name" => "NYPA_Maragal_7_huber",
     
     "res-norm"     => Inf,
-    "max-iter"     => 1000,
-    "rel-kkt-tol"  => 1e-10,
+    "max-iter"     => 50000,
+    "rel-kkt-tol"  => 1e-12,
     
     "acceleration"    => :none,
-    "accel-memory"    => 200,
+    "accel-memory"    => 50,
     "krylov-operator" => :tilde_A,
     
-    "anderson-broyden-type" => Symbol(1), # in {Symbol(1), :normal2, :QR2}
+    "anderson-broyden-type" => :normal2, # in {Symbol(1), :normal2, :QR2}
     "anderson-mem-type"     => :rolling, # in {:rolling, :restarted}
     "anderson-reg"          => :none, # in {:none, :tikonov, :frobenius}
     "anderson-period"       => 2,
@@ -27,12 +28,12 @@ config = Dict(
     "linesearch-period" => Inf,
     "linesearch-eps"    => 0.001,
 
-    "print-mod"          => 50,
+    "print-mod"          => 3000,
     "print-res-rel"      => true, # print relative (or absolute) residuals/duality gaps
     "show-vlines"        => true,
     "run-fast"           => false,
-    "global-timeout"     => 60.0, # seconds, including set-up time
-    "loop-timeout"       => 30.0, # seconds, loop excluding set-up time
+    "global-timeout"     => Inf, # seconds, including set-up time
+    "loop-timeout"       => Inf, # seconds, loop excluding set-up time
 );
 
 # run everything with a single call:
@@ -43,5 +44,5 @@ ws, results, to, x_ref, y_ref = FOMPrototypes.main(config);
 
 # for just prototype:
 # problem = FOMPrototypes.fetch_data(config["problem-set"], config["problem-name"]);
-# ws, results = FOMPrototypes.run_prototype(problem, config);
+# ws, results, to = FOMPrototypes.run_prototype(problem, config["problem-set"], config["problem-name"], config);
 ;
--- scripts/main_terminal.jl
+++ scripts/main_terminal.jl
@@ -6,13 +6,22 @@ if abspath(PROGRAM_FILE) == @__FILE__
     config = FOMPrototypes.parse_command_line()
     println()
 
-    println("first run:")
-    FOMPrototypes.main(config)
+    warmup_config = deepcopy(config)
+    warmup_config["problem-set"] = "sslsq"
+    warmup_config["problem-name"] = "NYPA_Maragal_3_huber"
+    warmup_config["max-iter"] = 300
+    warmup_config["global-timeout"] = 10.0
+    warmup_config["loop-timeout"] = 10.0
+    warmup_config["rel-kkt-tol"] = 0.0
 
-    println("second run:")
-    FOMPrototypes.main(config)
+    println("WARMUP run:")
+    problem = FOMPrototypes.fetch_data(warmup_config["problem-set"], warmup_config["problem-name"]);
+    ws, results, to = FOMPrototypes.run_prototype(problem, warmup_config["problem-set"], warmup_config["problem-name"], warmup_config);
 
-    println("third run:")
-    config["problem-name"] = "NYPA_Maragal_5_lasso"
-    FOMPrototypes.main(config)
+    println("--------------------")
+    println("--------------------")
+
+    println("ACTUAL run:")
+    problem = FOMPrototypes.fetch_data(config["problem-set"], config["problem-name"]);
+    ws, results, to = FOMPrototypes.run_prototype(problem, config["problem-set"], config["problem-name"], config);
 end
