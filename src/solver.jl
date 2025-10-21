2e306b295c25e8043b12b7acb423ae596cc22128
Gabriel Pereira <berkpereira@gmail.com>
2025-10-21 14:42:18 +0100

    Lifts vanilla step logic out of solver.jl
    Moves logic into vanilla_step! in src/alg/vanilla.jl. tried OK

--- src/solver.jl
+++ src/solver.jl
@@ -462,23 +462,7 @@ function optimise!(
         elseif args["acceleration"] == :anderson
             anderson_step!(ws, ws_diag, args, record, full_diagnostics, timer)
         else # no acceleration!
-            # copy older iterate before iterating
-            ws.vars.xy_prev .= ws.vars.xy
-
-            # TODO sort carefully what to with these separate scratch vectors
-            onecol_method_operator!(ws, ws.vars.xy, ws.scratch.swap_vec, true)
-            # swap contents of ws.vars.xy and ws.scratch.swap_vec
-            custom_swap!(ws.vars.xy, ws.scratch.swap_vec, ws.scratch.temp_mn_vec1)
-            # now ws.vars.xy contains newer iterate,  while
-            # ws.scratch.swap_vec contains older one
-
-            if !args["run-fast"]
-                curr_xy_update .= ws.vars.xy - ws.scratch.swap_vec
-
-                # record iteration data here
-                push!(record.xy_step_norms, norm(curr_xy_update))
-                push!(record.xy_step_char_norms, char_norm_func(curr_xy_update))
-            end
+            vanilla_step!(ws, args, record)
         end
         
         if !args["run-fast"]
